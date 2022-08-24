70646a71786151182f0065d40e120d76b44c92dd
RyanD64 <3625@holbertonschool.com>
2022-08-24 11:29:11 +0000

    test

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+"""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """"""
+    date = time.strftime('%Y%m%d%H%M%S')
+    arc = 'versions/web_static_' + date + '.tgz'
+
+    local('mkdir -p versions/')
+    res = local('tar -cvzf {} web_static/'.format(arc))
+
+    try:
+        return res
+    except Exception:
+        return None
