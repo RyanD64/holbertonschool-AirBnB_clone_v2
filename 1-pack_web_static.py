5375b6be8e3e082f71abaf0a641e9c61d06dffb5
RyanD64 <3625@holbertonschool.com>
2022-08-25 07:18:08 +0000

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-
-"""
+""""script that generate a .tgz archive from the contents of the web_static"""
 from fabric.api import local
 import time
 
 
 def do_pack():
-    """"""
+    """generate the archive"""
     date = time.strftime('%Y%m%d%H%M%S')
     arc = 'versions/web_static_' + date + '.tgz'
 
